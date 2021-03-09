4a6aff0f0c8050987bf5d0d92defa650c53f4508
Jadon T Schuler <jtschuler007@gmail.com>
2021-03-09 14:31:40 -0500

    modified solution

--- Project 2/src/project2.c
+++ Project 2/src/project2.c
@@ -42,75 +42,57 @@ int main(int argc, char* argv[]) {
 
     if (pid > 0) {
         // in parent
-        printf("In parent\n");
-
         // Open file
         int file = open(file_path, O_RDONLY);
         if (file == -1) {
-            perror("Error opening provided file.");
+            perror("Error opening provided file.\n");
             kill(pid, SIGKILL);
             exit(1);
         }
 
-        // Create buffer to use with reading file
+        // Create buffer to use with reading file and writing to stdout
         char buffer[BUF_SIZE];
+        for (int i = 0; i < BUF_SIZE; ++i) {
+            buffer[i] = '\0';
+        }
+        char read_buffer[BUF_SIZE];
+        for (int i = 0; i < BUF_SIZE; ++i) {
+            read_buffer[i] = '\0';
+        }
 
         // Read data from file into socket
-        ssize_t bytes_read, bytes_written;
-        while ((bytes_read = read(file, &buffer, BUF_SIZE)) > 0) {
-            bytes_written = write(sv[0], &buffer, bytes_read);
-            if (bytes_written != bytes_read) {
-                perror("Error while writing data to socket");
+        ssize_t bytes_read_file, bytes_read_socket, bytes_written;
+        while ((bytes_read_file = read(file, &buffer, BUF_SIZE)) > 0) {
+            bytes_written = write(sv[0], &buffer, bytes_read_file);
+            if (bytes_written != bytes_read_file) {
+                perror("Error while writing data to socket\n");
                 kill(pid, SIGKILL);
                 exit(1);
             }
+            // TODO
+            bytes_read_socket = read(sv[0], &read_buffer, BUF_SIZE);
+            write(1, &read_buffer, bytes_read_socket);
         }
 
         // Send end of file
-        write(sv[0], "\0", 1);
-        printf("Parent finished writing to socket\n");
+        write(sv[0], "\n\0", 2);
         close(file);
-
-        // Wait for child, and check exit status
-        printf("Parent waiting on child...\n");
-        int status;
-        wait(&status);
-        if (status != 0) {
-            perror("Error in child! Parent exiting...\n");
-            exit(1);
-        }
-        printf("\nChild process reaped.\n");
+        printf("Finished writing");
 
         // Print lines written to socket
-        printf("Parent continuing...\n");
         printf("\nMatching Lines:\n");
-        while ((bytes_read = read(sv[0], &buffer, BUF_SIZE)) > 0) {
-            write(1, &buffer, bytes_read);
-            if (buffer[bytes_read - 1] == '\0') {
+        while ((bytes_read_socket = read(sv[0], &buffer, BUF_SIZE)) > 0) {
+            write(1, &buffer, bytes_read_socket);
+            if (buffer[bytes_read_socket - 1] == '\0') {
                 break;
             }
         }
 
     } else {
         // in child
-        printf("\nIn child\n");
-        printf("\nReading data from socket stream...\n");
-
-        // Read from socket
-        char buffer[BUF_SIZE];
-        ssize_t bytes_read;
-        while ((bytes_read = read(sv[1], &buffer, BUF_SIZE)) > 0) {
-            printf("\n%ld bytes read:\n", bytes_read);
-            write(1, &buffer, bytes_read);
-            if (buffer[bytes_read - 1] == '\0') {
-                break;
-            }
-        }
-        printf("\n");
 
         // regex building
         // length: 4 + 4*len(target) + 1 (for null char)
-        printf("\nTarget: '%s'\n", target_word);
         int size = 5 + 4*strlen(target_word);
         char pattern[size];
         strcpy(pattern, "\\b");
@@ -125,38 +107,58 @@ int main(int argc, char* argv[]) {
             strcat(pattern, add);
         }
         strcat(pattern, "\\b\0");
-
-        printf("Regex: %s\n", pattern);
         regex_t regex;
-        if (regcomp(&regex, pattern, 0) == 0) {
-            printf("Regex compilation successful\n\n");
-        } else {
+        if (regcomp(&regex, pattern, 0) != 0) {
             perror("Regex compilaton failure\n");
             exit(1);
         }
 
-        // Check lines and write to socket
-        ssize_t bytes_written, line_length;
-        char * line;
-        char * state;
-        line = strtok_r(buffer, "\n", &state);
-        do {
-            // Match
-            if (regexec(&regex, line, 0, NULL, 0) == 0) {
-                printf("Match on '%s'\n", line);
-                // write data to socket
-                line_length = strlen(line);
-                bytes_written = write(sv[1], line, line_length);
-                if (bytes_written != line_length) {
-                    perror("Error while writing data to socket\n");
-                    regfree(&regex);
-                    exit(1);
+        // Read from socket
+        char buffer[BUF_SIZE];
+        for (int i = 0; i < BUF_SIZE; ++i) {
+            buffer[i] = '\0';
+        }
+        ssize_t bytes_read, bytes_written, line_length;
+        char line[BUF_SIZE];
+        for (int i = 0; i < BUF_SIZE; ++i) {
+            line[i] = '\0';
+        }
+        int start = 0;
+        int line_finished = 0;
+        while ((bytes_read = read(sv[1], &buffer, BUF_SIZE)) > 0) {
+            // Break on lines and match pattern
+            write(1, &buffer, bytes_read);
+            for (int i = 0; i < bytes_read; ++i) {
+                if (buffer[i] == '\n') {
+                    if (line_finished == 1)
+                        strncpy(line, buffer+start, i - start);
+                    else
+                        strncat(line, buffer+start, i - start);
+                    line_finished = 1;
+                    printf("Printing line: %s\n", line);
+                    line_length = strlen(line);
+                    if (regexec(&regex, line, 0, NULL, 0) == 0) {
+                        // write data to socket
+                        bytes_written = write(sv[1], line, line_length);
+                        write(sv[1], "\n", 1);
+                        if (bytes_written != line_length) {
+                            perror("Error while writing data to socket\n");
+                            regfree(&regex);
+                            exit(1);
+                        }
+                    }
+                    for (int j = 0; j < i - start; ++j) {
+                        line[j] = '\0';
+                    }
+                    start = i + 1;
                 }
-                write(sv[1], "\n", 1);
-            } else {
-                printf("No match on '%s'\n", line);
             }
-        } while ((line = strtok_r(NULL, "\n", &state)));
+
+            if (buffer[bytes_read - 1] == '\0') {
+                printf("end encountered\n");
+                break;
+            }
+        }
 
         // Write end of file
         write(sv[1], "\0", 1);
@@ -165,7 +167,19 @@ int main(int argc, char* argv[]) {
         regfree(&regex);
     }
 
-    if (pid == 0)
-        printf("Child process ended.\n");
+    if (pid == 0) {
+        close(sv[1]);
+        printf("Child done\n");
+    } else {
+        close(sv[0]);
+        int status;
+        wait(&status);
+        if (status != 0) {
+            perror("Error in child!\n");
+            exit(1);
+        }
+
+        printf("\nDone.\n");
+    }
     return 0;
 }
